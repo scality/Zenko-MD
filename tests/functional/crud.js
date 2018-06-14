e02c7ff092749c1cf9992327aa16b0f95d69eb64
Guillaume Gimenez <ploki@blackmilk.fr>
2018-06-13 17:18:23 -0700

    chore: Zenko-MD on eve

--- tests/functional/crud.js
+++ tests/functional/crud.js
@@ -1,57 +1,36 @@
-'use strict';
+'use strict'; // eslint-disable-line strict
 const assert = require('assert');
 const request = require('request');
 const async = require('async');
 
-const Bucket = "test";
+const Bucket = 'test';
 const bucketInfo = {
     acl: {
-        Canned : "private",
+        Canned: 'private',
         FULL_CONTROL: [],
         WRITE: [],
         WRITE_ACP: [],
         READ: [],
-        READ_ACP: []
+        READ_ACP: [],
     },
-    name : Bucket,
-    owner : "9d8fe19a78974c56dceb2ea4a8f01ed0f5fecb9d29f80e9e3b84104e4a3ea520",
-    ownerDisplayName: "anonymousCoward",
-    creationDate: "2018-06-04T17:45:42.592Z",
+    name: Bucket,
+    owner: '9d8fe19a78974c56dceb2ea4a8f01ed0f5fecb9d29f80e9e3b84104e4a3ea520',
+    ownerDisplayName: 'anonymousCoward',
+    creationDate: '2018-06-04T17:45:42.592Z',
     mdBucketModelVersion: 8,
     transient: false,
     deleted: false,
     serverSideEncryption: null,
     versioningConfiguration: null,
     websiteConfiguration: null,
-    locationConstraint: "us-east-1",
-    readLocationConstraint: "us-east-1",
+    locationConstraint: 'us-east-1',
+    readLocationConstraint: 'us-east-1',
     cors: null,
     replicationConfiguration: null,
     lifecycleConfiguration: null,
-    uid: "fea97818-6a9a-11e8-9777-e311618cc5d4"
+    uid: 'fea97818-6a9a-11e8-9777-e311618cc5d4',
 };
-const mdpUrl = "http://localhost:9000";
-
-
-function mdpPost(path, body, cb) {
-    request.post({
-        url: mdpUrl+path,
-        json: true,
-        body,
-    }, cb);
-}
-function mdpGet(path, cb) {
-    request.get({
-        url: mdpUrl+path,
-        json: true
-    }, cb);
-}
-function mdpDelete(path, cb) {
-    request.delete({
-        url: mdpUrl+path,
-        json: true
-    }, cb);
-}
+const mdpUrl = 'http://localhost:9000';
 
 const objects = [
     'aaa',
@@ -62,10 +41,32 @@ const objects = [
     'ddd',
 ];
 
+const dispatcher = {
+    post: (path, body, cb) => {
+        request.post({
+            url: mdpUrl + path,
+            json: true,
+            body,
+        }, cb);
+    },
+    get: (path, cb) => {
+        request.get({
+            url: mdpUrl + path,
+            json: true,
+        }, cb);
+    },
+    delete: (path, cb) => {
+        request.delete({
+            url: mdpUrl + path,
+            json: true,
+        }, cb);
+    },
+};
+
 function _getExpectedListing(prefix, objects) {
     const filtered = objects.map(key => {
         const deprefixed = key.slice(prefix.length);
-        return deprefixed.replace(/[/].*/,'/');
+        return deprefixed.replace(/[/].*/, '/');
     });
     const keySet = {};
     return filtered.filter(key => {
@@ -81,58 +82,60 @@ function _getExpectedListing(prefix, objects) {
 }
 
 function _listingURL(prefix, marker) {
-    const re_slash = /[/]/g;
-    return '/default/bucket/' + Bucket +
-        '?delimiter=%2F&prefix=' +
-        prefix.replace(re_slash, '%2F') +
-        '&maxKeys=1&marker=' +
-        marker.replace(re_slash, '%2F');
+    const reSlash = /[/]/g;
+    const escapedPrefix = prefix.replace(reSlash, '%2F');
+    const escapedMarker = marker.replace(reSlash, '%2F');
+    return `/default/bucket/${Bucket}?delimiter=%2F&prefix=` +
+        `${escapedPrefix}&maxKeys=1&marker=${escapedMarker}`;
 }
+
 function _listObjects(prefix, objects, cb) {
     const keys = _getExpectedListing(prefix, objects);
     const markers = keys.slice(0);
     markers.unshift(undefined);
     const lastKey = keys[keys.length - 1];
-    const listing = keys.map((key, index) => {
-        return {
-            key,
-            marker: markers[index],
-            NextMarker: markers[index + 1],
-            IsTruncated: key!=lastKey,
-            isPrefix: key.endsWith('/')
-        };
-    });
+    const listing = keys.map((key, index) => ({
+        key,
+        marker: markers[index],
+        NextMarker: markers[index + 1],
+        IsTruncated: key !== lastKey,
+        isPrefix: key.endsWith('/'),
+    }));
     async.mapLimit(listing, 5, (obj, next) => {
         const currentMarker = obj.marker === undefined ? '' : obj.marker;
-        mdpGet(_listingURL(prefix, prefix + currentMarker),
-            (err, response, body) => {
-                if (err) {
-                    return next(err);
-                }
-                if (obj.isPrefix) {
-                    assert.strictEqual(body.Contents.length, 0);
-                    assert.strictEqual(body.CommonPrefixes.length, 1);
-                    assert.strictEqual(body.CommonPrefixes[0], prefix + obj.key);
-                } else {
-                    assert.strictEqual(body.Contents.length, 1);
-                    assert.strictEqual(body.CommonPrefixes.length, 0);
-                    assert.strictEqual(body.Contents[0].key, prefix + obj.key);
-                }
-                assert.strictEqual(body.IsTruncated, obj.IsTruncated);
-                if (body.IsTruncated) {
-                    assert.strictEqual(body.NextMarker, prefix + obj.NextMarker);
-                }
-                return next();
-        });
-    }, err => {
-        return cb(err);
-    });
+        dispatcher.get(_listingURL(prefix, prefix + currentMarker),
+                       (err, response, body) => {
+                           if (err) {
+                               return next(err);
+                           }
+                           if (obj.isPrefix) {
+                               assert.strictEqual(body.Contents.length, 0);
+                               assert.strictEqual(body.CommonPrefixes.length,
+                                                  1);
+                               assert.strictEqual(body.CommonPrefixes[0],
+                                                  prefix + obj.key);
+                           } else {
+                               assert.strictEqual(body.Contents.length, 1);
+                               assert.strictEqual(body.CommonPrefixes.length,
+                                                  0);
+                               assert.strictEqual(body.Contents[0].key,
+                                                  prefix + obj.key);
+                           }
+                           assert.strictEqual(body.IsTruncated,
+                                              obj.IsTruncated);
+                           if (body.IsTruncated) {
+                               assert.strictEqual(body.NextMarker,
+                                                  prefix + obj.NextMarker);
+                           }
+                           return next();
+                       });
+    }, err => cb(err));
 }
 
 function _createObjects(objects, cb) {
     async.mapLimit(objects, 5, (key, next) => {
-        mdpPost('/default/bucket/'+Bucket+'/'+key,
-        {key}, next);
+        dispatcher.post(`/default/bucket/${Bucket}/${key}`,
+                        { key }, next);
     }, err => {
         cb(err);
     });
@@ -140,11 +143,11 @@ function _createObjects(objects, cb) {
 
 function _readObjects(objects, cb) {
     async.mapLimit(objects, 5, (key, next) => {
-        mdpGet('/default/bucket/'+Bucket+'/'+key,
-        (err, response, body) => {
-            assert.deepStrictEqual(body.key, key);
-            next(err);
-        });
+        dispatcher.get(`/default/bucket/${Bucket}/${key}`,
+                       (err, response, body) => {
+                           assert.deepStrictEqual(body.key, key);
+                           next(err);
+                       });
     }, err => {
         cb(err);
     });
@@ -152,99 +155,78 @@ function _readObjects(objects, cb) {
 
 function _deleteObjects(objects, cb) {
     async.mapLimit(objects, 5, (key, next) => {
-        mdpDelete('/default/bucket/'+Bucket+'/'+key,
-        err => {
-            next(err);
-        });
+        dispatcher.delete(`/default/bucket/${Bucket}/${key}`,
+                          err => next(err));
     }, err => {
         cb(err);
     });
 }
 
 describe('Basic Metadata Proxy Server test',
- function bindToThis() {
-    this.timeout(10000);
-    it('Shoud get the metadataInformation', done => {
-        mdpGet('/default/metadataInformation',
-        (err, response, body) => {
-            if (err) {
-                return done(err);
-             }
-            assert.deepStrictEqual(body, {metadataVersion: 2});
-            done();
-        });
-    });
-});
+         function bindToThis() {
+             this.timeout(10000);
+             it('Shoud get the metadataInformation', done => {
+                 dispatcher.get('/default/metadataInformation',
+                                (err, response, body) => {
+                                    if (err) {
+                                        return done(err);
+                                    }
+                                    assert.deepStrictEqual(
+                                        body, { metadataVersion: 2 });
+                                    return done();
+                                });
+             });
+         });
 
-describe('Basic Metadata Proxy Server CRUD test',
- function bindToThis() {
+describe('Basic Metadata Proxy Server CRUD test', function bindToThis() {
     this.timeout(10000);
+
     beforeEach(done => {
-        mdpPost('/default/bucket/'+Bucket, bucketInfo,
-            done);
+        dispatcher.post(`/default/bucket/${Bucket}`, bucketInfo,
+                        done);
     });
-    
+
     afterEach(done => {
-        //return done();
-        mdpDelete('/default/bucket/'+Bucket, done);
+        dispatcher.delete(`/default/bucket/${Bucket}`, done);
     });
 
     it('Should get the bucket attributes', done => {
-        mdpGet('/default/attributes/'+Bucket,
-        (err, response, body) => {
-            if (err) {
-                return done(err);
-            }
-            assert.deepStrictEqual(body.name,
-                        bucketInfo.name);
-            done();
-                 
-        });
+        dispatcher.get(`/default/attributes/${Bucket}`,
+                       (err, response, body) => {
+                           if (err) {
+                               return done(err);
+                           }
+                           assert.deepStrictEqual(body.name,
+                                                  bucketInfo.name);
+                           return done();
+                       });
     });
 
     it('Should crud an object', done => {
         async.waterfall([
-            next => {
-                mdpPost('/default/bucket/'+Bucket+'/test1',
-                        { foo: 'gabu' },
-                (err, response) => {
-                    next(err);
-                });
-            },
-            next => {
-                mdpGet('/default/bucket/'+Bucket+'/test1',
-                (err, response, body) => {
-                    if (!err) {
-                        assert.deepStrictEqual(body.foo, 'gabu');
-                        next(err);
-                    }
-                });
-            },
-            next => {
-                mdpPost('/default/bucket/'+Bucket+'/test1',
-                        { foo: 'zome' },
-                (err, response) => {
-                    next(err);
-                });
-            },
-            next => {
-                mdpGet('/default/bucket/'+Bucket+'/test1',
-                (err, response, body) => {
-                    if (!err) {
-                        assert.deepStrictEqual(body.foo, 'zome');
-                        next(err);
-                    }
-                });
-            },
-            next => {
-                mdpDelete('/default/bucket/'+Bucket+'/test1',
-            (err, response) => {
-                next(err);
-            });
-            }
-        ], err => {
-            done(err);
-        });
+            next => dispatcher.post(`/default/bucket/${Bucket}/test1`,
+                                    { foo: 'gabu' }, err => next(err)),
+            next => dispatcher.get(`/default/bucket/${Bucket}/test1`,
+                                   (err, response, body) => {
+                                       if (!err) {
+                                           assert.deepStrictEqual(body.foo,
+                                                                  'gabu');
+                                           next(err);
+                                       }
+                                   }),
+            next => dispatcher.post(`/default/bucket/${Bucket}/test1`,
+                                    { foo: 'zome' }, err => next(err)),
+            next => dispatcher.get(`/default/bucket/${Bucket}/test1`,
+                                   (err, response, body) => {
+                                       if (!err) {
+                                           assert.deepStrictEqual(body.foo,
+                                                                  'zome');
+                                           next(err);
+                                       }
+                                   }),
+            next => dispatcher.delete(`/default/bucket/${Bucket}/test1`,
+                                      err => next(err)),
+        ], err => done(err));
     });
 
     it('Should list objects', done => {
